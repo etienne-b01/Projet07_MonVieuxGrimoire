f720826c02b8ce2ca7bf3303af421c77d17aea20
etienne-b01 <e.beuter@wanadoo.fr>
2024-10-14 15:42:03 +0200

    updated routes

--- routes/books.js
+++ routes/books.js
@@ -4,14 +4,19 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.get('/', auth, bookCtrl.getAllBooks);
+router.get('/books', auth, bookCtrl.getAllBooks);
 
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/api/books', auth, multer, bookCtrl.createBook);
 
-router.get('/:id', auth, bookCtrl.getOneBook);
+router.get('/api/books:id', auth, bookCtrl.getOneBook);
 
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.put('/api/books:id', auth, multer, bookCtrl.modifyBook);
 
-router.delete('/:id', auth, bookCtrl.deleteBook);
+router.delete('/api/books:id', auth, bookCtrl.deleteBook);
+
+//nvls routes
+//router.post('/api/books:id/rating', auth, bookCtrl.setRating);
+
+//router.get('/api/books/bestrating', bookCtrl.returnBestRated);
 
 module.exports = router;
